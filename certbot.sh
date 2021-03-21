3ab8e73efcf307f73be561a6f9fddda1d6e5633d
sam <sam@freighttrust.com>
2021-03-21 09:10:36 -0700

    feat(certbot): init

---
+++ certbot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+# sudo certbot --nginx
+sudo certbot certonly --nginx
